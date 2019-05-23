c207b2b6a3da03ce2c6879b09b4ca2dad550cdc8
liuyejunnan <liuyejunnan@didiglobal.com>
2019-05-23 17:09:59 +0800

    no message

--- 20190523/call.js
+++ 20190523/call.js
@@ -1,7 +1,7 @@
 const app = {
     version: '1.0.0',
     getVersion() {
-        console.log(this.version)
+        console.log(this)
     }
 };
 
@@ -10,9 +10,9 @@ app.getVersion();
 app.getVersion.call({ version: '1.2.0' });
 
 
-Function.prototype.call2 = function (context = window, ...args) {
-    context.fn = this;
-    const result = context.fn(...args)
-    delete context.fn;
-    return result;
-}
+// Function.prototype.call2 = function (context = window, ...args) {
+//     context.fn = this;
+//     const result = context.fn(...args)
+//     delete context.fn;
+//     return result;
+// }
