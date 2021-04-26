62b544abf9549acb53377fc916f3a4a3ac07fad9
liuyejunnan <liuyejunnan@didiglobal.com>
2021-04-26 10:50:49 +0800

    no message

---
+++ leetcode/rgbTohex.js
@@ -0,0 +1,14 @@
+const white = "rgb(225, 225, 225)";
+
+const RGB2HEX = (color) => {
+    let rgbString = color.replace('rgb(', '').replace("?", "");
+    let formateRGB = rgbString.split(",");
+    let formateHEX = [];
+    formateRGB.map((col) => {
+        formateHEX.push(parseInt(col, 16));
+    });
+    return formateHEX.join("");
+}
+
+const Hex = RGB2HEX(white);
+console.log(Hex);
